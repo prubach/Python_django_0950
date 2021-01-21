0fff38f7697d558d21cd497a926329da67614f45
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-01-21 10:22:16 +0100

    Added template to display customer data from the database

--- bank/bankapp/urls.py
+++ bank/bankapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('hello', views.hello, name='Hello')
+    path('hello', views.hello, name='Hello'),
+    path('<int:cust_id>/', views.show_cust, name='Customer')
 ]
--- bank/bankapp/views.py
+++ bank/bankapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import Customer
+
 
 def index(request):
     return HttpResponse('Welcome to Bank Application!')
@@ -10,3 +12,8 @@ def index(request):
 def hello(request):
     msg = 'Hello Text from a template'
     return render(request, 'test.html', {'hello_text': msg})
+
+
+def show_cust(request, cust_id):
+    customer = get_object_or_404(Customer, pk=cust_id)
+    return render(request, 'cust.html', {'customer': customer})
