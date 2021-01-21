a0e940c3128fc8916f4df10174c281bbd4ab51b2
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-01-21 11:16:51 +0100

    REST framework added -

--- bank/bankapp/urls.py
+++ bank/bankapp/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path, include
 from . import views
 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'customers', views.CustomerViewSet)
+router.register(r'accounts', views.AccountViewSet)
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('hello', views.hello, name='Hello'),
+    path('', include(router.urls)),
     path('<int:cust_id>/', views.show_cust, name='Customer')
 ]
--- bank/bankapp/views.py
+++ bank/bankapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
 
 # Create your views here.
-from .models import Customer
+from rest_framework import viewsets
 
+from .models import Customer, Account
+from .serializers import CustomerSerializer, AccountSerializer, UserSerializer, GroupSerializer
 
 def index(request):
     return HttpResponse('Welcome to Bank Application!')
@@ -17,3 +20,23 @@ def hello(request):
 def show_cust(request, cust_id):
     customer = get_object_or_404(Customer, pk=cust_id)
     return render(request, 'cust.html', {'customer': customer})
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
