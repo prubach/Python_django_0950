0d97213829cddf1f7ceb55e75592bf73a5b18418
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-01-21 10:11:42 +0100

    First template with text from python

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ http://127.0.0.1:8000/admin
 
 user: admin
 pass: 123
+
+Tag filled by python code: http://127.0.0.1:8000/hello
--- bank/bankapp/views.py
+++ bank/bankapp/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Welcome to Bank Application!')
+
+
+def hello(request):
+    msg = 'Hello Text from a template'
+    return render(request, 'test.html', {'hello_text': msg})
